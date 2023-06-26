14fbbebc4e099cb5c57b2d4395f7fb6cf25a751f
divo <divoapple@gmail.com>
2023-06-26 21:20:51 +0100

    Initial

---
+++ Daily Planner/Daily_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_PlannerApp.swift
+//  Daily Planner
+//
+//  Created by Steven Diviney on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Daily_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
