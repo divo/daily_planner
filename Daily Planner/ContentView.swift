3c2d1f5ce89961365b241e9f29a9157f0a9b6f5e
divo <divoapple@gmail.com>
2023-06-26 22:01:22 +0100

    Dynamic view building poc

--- Daily Planner/ContentView.swift
+++ Daily Planner/ContentView.swift
@@ -7,20 +7,59 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+struct Element: Identifiable {
+  let id = UUID()
+  let data: String
+  let type: String
+  
+  func buildView() -> AnyView {
+    switch type {
+    case "Button": return AnyView( Button(data, action: {
+      print("Press")
+    }))
+    case "Text": return AnyView( Text(data) )
+    default: return AnyView(EmptyView())
     }
+  }
+}
+
+struct ContentView : View {
+  @State var elements = [
+    Element(data: "John", type: "Text"),
+    Element(data: "Alice", type: "Button"),
+    Element(data: "Bob", type: "Text")
+  ]
+  
+  // 2
+  var body: some View {
+    List {
+      ForEach(0..<$elements.count, id: \.self) { index in
+        elements[index].buildView()
+      }
+    }
+    
+    HStack {
+      Button {
+        elements.append(Element(data: "Hi", type: "Text"))
+      } label: {
+        Text("Append text")
+      }
+      
+      Button {
+        elements.append(Element(data: "Test", type: "Button"))
+      } label: {
+        Text("Append button")
+      }
+
+      
+    }
+    
+  }
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
